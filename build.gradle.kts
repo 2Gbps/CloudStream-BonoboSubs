faa14ed98e6de1bd7d6298bf0afdf1b52ba1fe43
2Gbps <2Gbps@users.noreply.github.com>
2026-09-08 12:25:49 -0300

    Pin cloudstream gradle plugin to JitPack build -32895aedb6-1 (snapshot cache flake)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,9 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        // Cloudstream gradle plugin which makes everything work and builds plugins
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        // Cloudstream gradle plugin, pinned to a concrete JitPack build
+        // (the -SNAPSHOT resolution flakes when JitPack's cache expires)
+        classpath("com.github.recloudstream:gradle:-32895aedb6-1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.20")
     }
 }
